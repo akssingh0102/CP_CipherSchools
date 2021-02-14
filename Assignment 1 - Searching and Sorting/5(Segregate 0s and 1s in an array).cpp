148100e97732e3c028634c983251ac8cfe778a66
Akash <akash@Akashs-MacBook-Pro.local>
2021-02-14 21:55:18 +0530

    fifth

--- Assignment 1 - Searching and Sorting/5(Segregate 0s and 1s in an array).cpp
+++
@@ -1,2 +0,0 @@
-//*******************************Segregate 0s and 1s in an array****************************
-//https://www.geeksforgeeks.org/segregate-0s-and-1s-in-an-array-by-traversing-array-once/
