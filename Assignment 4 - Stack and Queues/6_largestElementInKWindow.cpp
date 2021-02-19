aee83fa1ee72157c0ead45623b89b738251e5a3e
Akash <akash@Akashs-MacBook-Pro.local>
2021-02-19 16:15:58 +0530

    push

---
+++ Assignment 4 - Stack and Queues/6_largestElementInKWindow.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
+
+#define ll                  long long int 
+#define llmin               -1e18
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define vi                  vector<int>
+#define take(a,n)           vector <int> a; for(int i=0;i<n;i++){int aa; cin>>aa; a.pb(aa);}
+#define vll                 vector<ll> 
+#define full(a)             (a.begin(),a.end())
+#define vc                  vector<char>
+#define iz(n)               int n; cin>>n;
+#define iz2(n,m)             int n,m; cin>>n>>m;
+#define mii                 map<int,int>
+#define setbits(x)          __builtin_popcountll(x)
+#define zerobits(x)         __builtin_ctzll(x)    //zeros before first 1
+#define com                 int t; cin>>t; while(t--)
+#define forn(n)             for(int i=0;i<n;i++)
+#define fo(x,y)             for(int i=x;i<y;i++)
+#define pq                  priority_queue <int, vector<int>, greater<int> >
+
+void printMaxK(int *arr,int n,int k){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    int arr[] = { 12, 1, 78, 90, 57, 89, 56 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+    printMaxintK(arr,n,k);
+    return 0;
+}
